ef91cd8fa229b4ef4c5346e32f41004460a24114
NS NooR <non377938@gmail.com>
2024-08-15 20:24:58 +0600

    splash

---
+++ lib/views/screens/splash/splash.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:laundry_service_app/utils/const/color.dart';
+import 'package:laundry_service_app/utils/const/images.dart';
+import 'package:laundry_service_app/views/Widgets/custom_text.dart';
+import 'package:laundry_service_app/views/screens/welcome/welcome.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => const WelcomePage())));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CommonColor.skyBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(CommonImage.bbeLogo),
+            const SizedBox(height: 30,),
+            const CustomText()
+          ],
+        ),
+      ),
+    );
+  }
+}
