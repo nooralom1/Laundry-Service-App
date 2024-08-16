90ee12bb79ad22418fa04909a56c2ace21cf9bcb
NS NooR <non377938@gmail.com>
2024-08-16 17:53:54 +0600

    price

---
+++ lib/models/price.dart
@@ -0,0 +1,36 @@
+class PriceModel {
+  final String name;
+  final String imagePath;
+  final String? p1;
+  final String? p2;
+  final String? p3;
+  final String? p4;
+  final String? p5;
+  final String? p6;
+  final String? p7;
+  final String? p8;
+  final String? p9;
+  final String? p10;
+  final String? p11;
+  final String? p12;
+  PriceModel(
+      {required this.name,
+      required this.imagePath,
+      this.p1,
+      this.p2,
+      this.p3,
+      this.p4,
+      this.p5,
+      this.p6,
+      this.p7,
+      this.p8,
+      this.p9,
+      this.p10,
+      this.p11,
+      this.p12});
+}
+List<PriceModel> priceData = [
+  PriceModel(name: "Wears", imagePath: "assets/images/Rectangle 14.png"),
+  PriceModel(name: "Home & Bedding", imagePath: "assets/images/Rectangle 16.1.png"),
+  PriceModel(name: "Commercial ", imagePath: "assets/images/Rectangle 22.png"),
+];
