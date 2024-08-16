155316e167b25ba17ed86b7c7678eda86290b81f
NS NooR <non377938@gmail.com>
2024-08-16 18:38:15 +0600

    service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:laundry_service_app/views/screens/prices/price.dart';
+import 'package:laundry_service_app/views/screens/service/service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PricePage(),
+      home: ServicePage(),
     );
   }
 }
--- lib/views/screens/service/service.dart
+++ lib/views/screens/service/service.dart
@@ -13,74 +13,152 @@ class _ServicePageState extends State<ServicePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: CommonColor.skyBlue,
-        appBar: AppBar(
-          leading: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: const Icon(
-                Icons.arrow_back,
-                color: CommonColor.white,
-              )),
-          title: const Text(
-            "Services",
-            style: TextStyle(color: CommonColor.white),
-          ),
-          centerTitle: true,
-          backgroundColor: CommonColor.blue,
+      backgroundColor: CommonColor.skyBlue,
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              color: CommonColor.white,
+            )),
+        title: const Text(
+          "Services",
+          style: TextStyle(color: CommonColor.white),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: ListView.builder(
-              itemCount: serviceData.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: Container(
-                    height: 150,
-                    color: CommonColor.white,
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          serviceData[index].imagePath,
-                          height: 150,
-                          width: 114,
-                          fit: BoxFit.fill,
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                         Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              serviceData[index].serviceName,
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w600),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              serviceData[index].serviceDetails,
-                              style: const TextStyle(
-                                  fontSize: 13,),
-                            ),
-                            const SizedBox(height: 15,),
-                            Row(
-                              children: [
-                                Text(serviceData[index].serviceCharge,style: const TextStyle(color: CommonColor.blue),),
-                                Icon(serviceData[index].rightArrow,color: CommonColor.blue,)
-                              ],
-                            ),
-                          ],
-                        )
-                      ],
+        centerTitle: true,
+        backgroundColor: CommonColor.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: serviceData.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Container(
+                      height: 150,
+                      color: CommonColor.white,
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            serviceData[index].imagePath,
+                            height: 150,
+                            width: 114,
+                            fit: BoxFit.fill,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                serviceData[index].serviceName,
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                serviceData[index].serviceDetails,
+                                style: const TextStyle(
+                                  fontSize: 13,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    serviceData[index].serviceCharge,
+                                    style: const TextStyle(color: CommonColor.blue),
+                                  ),
+                                  Icon(
+                                    serviceData[index].rightArrow,
+                                    color: CommonColor.blue,
+                                  )
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              height: 150,
+              width: double.maxFinite,
+              color: CommonColor.white,
+              child: const Column(
+                children: [
+                  SizedBox(height: 5,),
+                  Text("About Our Service",style: TextStyle(fontWeight: FontWeight.w600),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "* Folding and Packaging",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                           "* Alteration and repair",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            "* Commercial service",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            "* Rug and carpet cleaning",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "* Stain removal",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            "* Pick up and delivery",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            "* Home service",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          Text(
+                            "* Online tracking and payment",
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                );
-              }),
-        ));
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
