08c1a9345e5f436c4be963ed56ba99d29429d63c
NS NooR <non377938@gmail.com>
2024-08-20 00:12:23 +0600

    profile

--- lib/views/screens/service/widgets/listviewbuilder_widget.dart
+++ lib/views/screens/service/widgets/listviewbuilder_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:laundry_service_app/models/Service.dart';
 import 'package:laundry_service_app/utils/const/color.dart';
 
@@ -14,56 +15,61 @@ class ListViewBuilderWidget extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Container(
-            height: 150,
-            color: CommonColor.white,
-            child: Row(
-              children: [
-                Image.asset(
-                  serviceData[index].imagePath,
-                  height: 150,
-                  width: 114,
-                  fit: BoxFit.fill,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      serviceData[index].serviceName,
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.w600),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      serviceData[index].serviceDetails,
-                      style: const TextStyle(
-                        fontSize: 13,
+          child: InkWell(
+            onTap: (){
+
+            },
+            child: Container(
+              height: 150,
+              color: CommonColor.white,
+              child: Row(
+                children: [
+                  Image.asset(
+                    serviceData[index].imagePath,
+                    height: 150,
+                    width: 114,
+                    fit: BoxFit.fill,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        serviceData[index].serviceName,
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          serviceData[index].serviceCharge,
-                          style: const TextStyle(
-                              color: CommonColor.blue),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        serviceData[index].serviceDetails,
+                        style: const TextStyle(
+                          fontSize: 13,
                         ),
-                        Icon(
-                          serviceData[index].rightArrow,
-                          color: CommonColor.blue,
-                        )
-                      ],
-                    ),
-                  ],
-                )
-              ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            serviceData[index].serviceCharge,
+                            style: const TextStyle(
+                                color: CommonColor.blue),
+                          ),
+                          Icon(
+                            serviceData[index].rightArrow,
+                            color: CommonColor.blue,
+                          )
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         );
