52560f024dd9bdedbbec06cf4668cb784797fcb8
NS NooR <non377938@gmail.com>
2024-08-20 19:30:39 +0600

    verification

---
+++ lib/database/price.dart
@@ -0,0 +1,42 @@
+import 'package:laundry_service_app/models/price.dart';
+
+List<PriceModel> priceData = [
+  PriceModel(
+    name: "Wears",
+    imagePath: "assets/images/Rectangle 14.png",
+    p1: "* T-shirt on hanger  #2,500",
+    p2: "* 2-piece suit  #3,500",
+    p3: "* Short  #2,000",
+    p4: "* Dress  #2,500",
+    p5: "* Jeans  #2,500",
+    p6: "* Native wear  #2,500",
+    p7: "* T-shirt folded  #3,000",
+    p8: "* 3-piece suit  #4,000",
+    p9: "* Pyjamas  #2,500",
+    p10: "* Single top  #3,000",
+    p11: "* Complete agbada  #3,000",
+    p12: "* Lace wears  #3,000",
+  ),
+  PriceModel(
+    name: "Home & Bedding",
+    imagePath: "assets/images/Rectangle 16.1.png",
+    p1: "* Bath towels  #2,500",
+    p2: "* Blankets  #3,500",
+    p3: "* Carpet/rug  #2,000",
+    p4: "* Mattress protector  #2,500",
+    p7: "* Duvet  #3,000",
+    p8: "* Bed spread  #4,000",
+    p9: "* Curtains  #2,500",
+    p10: "* Pillow covers  #3,000",
+  ),
+  PriceModel(
+    name: "Commercial ",
+    imagePath: "assets/images/Rectangle 22.png",
+    p1: "* Coveralls  #2,500",
+    p2: "* 2-piece suit  #3,500",
+    p3: "* Short  #2,000",
+    p7: "* Hotel Bedshits  #3,000",
+    p8: "* 3-piece suit  #4,000",
+    p9: "* Pyjamas  #2,500",
+  ),
+];
