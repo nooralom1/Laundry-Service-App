4566f5fce41d14f2438bdfac44e4c619cd84f10d
NS NooR <non377938@gmail.com>
2024-08-25 14:44:06 +0600

    verification

--- lib/utils/const/text.dart
+++ lib/utils/const/text.dart
@@ -1,5 +1,4 @@
-class CommonText{
-
+class CommonText {
   static const String customText = "LAUNDRY SERVICE";
   static const String login = "LOGIN";
   static const String signup = "SIGN UP";
@@ -14,22 +13,25 @@ class CommonText{
   static const String gglLgnT6 = "Clement Wealth";
   static const String gglLgnT7 = "sundaymoseschidozie@gmail.com";
   static const String gglLgnT8 = "Add another account";
-  static const String gglLgnT9 = "To continue, Google will share your name,\nemail address, and profile with \nBEE LAUNDRY. Before using this app, review";
+  static const String gglLgnT9 =
+      "To continue, Google will share your name,\nemail address, and profile with \nBEE LAUNDRY. Before using this app, review";
   static const String gglLgnT10 = "it’s ";
   static const String gglLgnT11 = "privacy policy";
   static const String gglLgnT12 = " and ";
   static const String gglLgnT13 = "terms of service";
 
   //Login Page
-  static const String loginT1 = "Start by using your email address &\n                      password";
+  static const String loginT1 =
+      "Start by using your email address &\n                      password";
   static const String loginT2 = "Forgot Password ?";
   static const String loginT3 = "Don't have a account?  ";
 
   //Signup Page
-  static const String signupT  = "Already have an account?  ";
+  static const String signupT = "Already have an account?  ";
 
   //forget password page
-  static const String forgetPassT = "Please enter your email to reset your\n                      password";
+  static const String forgetPassT =
+      "Please enter your email to reset your\n                      password";
 
   //Verification Page
   static const String verificationT1 = "Verification Code";
@@ -42,14 +44,17 @@ class CommonText{
   static const String creatNewPassT2 = "Confirm Password";
 
   //Welcome Page
-  static const String welcomeT1 = "Your Personal Laundry & Dry Cleaning Service\n     with Collection & Free Delivery in 24 hour";
+  static const String welcomeT1 =
+      "Your Personal Laundry & Dry Cleaning Service\n     with Collection & Free Delivery in 24 hour";
   static const String welcomeT2 = "Continue Wth Google";
 
   //Home Page
   static const String homeT1 = "WELCOME YOU CAN ORDER NOW.";
   static const String homeT2 = "DRY CLEANING & LAUNDRY\nSERVICES";
-  static const String homeT3 = "We always make sure our customers are happy and satified";
-  static const String homeT4 = "Discover the joy of wearing neatly laundered\nclothes that smell wonderful. ";
+  static const String homeT3 =
+      "We always make sure our customers are happy and satified";
+  static const String homeT4 =
+      "Discover the joy of wearing neatly laundered\nclothes that smell wonderful. ";
   static const String homeT5 = "See our services";
   static const String homeT6 = "See our prices";
 }
