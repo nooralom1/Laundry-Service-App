8a3614319663a5a29b681a460b130bf0e564fccc
NS NooR <non377938@gmail.com>
2024-08-25 14:44:13 +0600

    verification

--- lib/database/service.dart
+++ lib/database/service.dart
@@ -6,7 +6,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 14.png",
     serviceName: "Laundry Service",
     serviceDetails:
-    "This is regular laundry, items are\nwashed, tumble dried and neatly\nfolded garments. All order will be\ncharged based on weight",
+        "This is regular laundry, items are\nwashed, tumble dried and neatly\nfolded garments. All order will be\ncharged based on weight",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
@@ -14,7 +14,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 16.png",
     serviceName: "Clean & Iron",
     serviceDetails:
-    "This is washed and iron on \nhanger by default. Folding can\nbe requested additionally.",
+        "This is washed and iron on \nhanger by default. Folding can\nbe requested additionally.",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
@@ -22,7 +22,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 18.png",
     serviceName: "Folding & packaging",
     serviceDetails:
-    "This is ironing only on hanger by\ndefault. Folding can be \nrequested additionally.",
+        "This is ironing only on hanger by\ndefault. Folding can be \nrequested additionally.",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
@@ -30,7 +30,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 20.png",
     serviceName: "Dry Cleaning",
     serviceDetails:
-    "Dry cleaning items clean and\nironing,. Folding can be \nrequestes  additionally. ",
+        "Dry cleaning items clean and\nironing,. Folding can be \nrequestes  additionally. ",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
@@ -38,7 +38,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 22.png",
     serviceName: "Commercial cleaning",
     serviceDetails:
-    "The cleaning process varies \ndepending on the each item\nrequirement, some item may \nneed 72hours to complete.",
+        "The cleaning process varies \ndepending on the each item\nrequirement, some item may \nneed 72hours to complete.",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
@@ -46,7 +46,7 @@ List<ServiceModel> serviceData = [
     imagePath: "assets/images/Rectangle 24.png",
     serviceName: "Ironing/pressing",
     serviceDetails:
-    "Alteration requires a different \nprocess. Priced per item up to\n72hours",
+        "Alteration requires a different \nprocess. Priced per item up to\n72hours",
     serviceCharge: "Check prices",
     rightArrow: CupertinoIcons.right_chevron,
   ),
